3cb8fcbd4cb5d931f9904b9f5401e25a5308136b
oktr <oktatas.rekasi@gmail.com>
2024-04-03 09:40:46 +0200

    Alapproject

---
+++ src/FajlToDatabase.java
@@ -0,0 +1,8 @@
+import controller.DatabaseController;
+
+public class FajlToDatabase {
+    public static void main(String[] args) {
+        
+        new DatabaseController();
+    }
+}
---
+++ src/controller/DatabaseController.java
@@ -0,0 +1,9 @@
+package controller;
+
+public class DatabaseController {
+
+    public DatabaseController() {
+
+        System.out.println("Hello");
+    }
+}
